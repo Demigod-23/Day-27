e1f01e23278eaf1689cdcaf95728c1ac23881925
AnK <AnK@DESKTOP-35CEPU1>
2022-08-12 04:27:12 +0530

    day 27 code

---
+++ Day 27/src/com/ank/generics/g1.java
@@ -0,0 +1,17 @@
+package com.ank.generics;
+import java.util.*;  
+class g1{  
+public static void main(String args[]){  
+ArrayList<String> list=new ArrayList<String>();  
+list.add("Ankesh");  
+list.add("Gupta");  
+  
+String s=list.get(1); 
+System.out.println("element is: "+s);  
+  
+Iterator<String> itr=list.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
